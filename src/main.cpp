6bf9c3d5c13b199a1ab05b8e0fc29a9eee48ac82
Odd Kristian Kvarmestol <okknor@gmail.com>
2018-01-29 16:18:52 +0100

    Implement assertion

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,23 @@ void panic(const char *fmt, ...) {
 	exit(-1);
 }
 
+bool _assert(bool condition, const char *msg, const char *cond_str, const char *file, const char *func, size_t line) {
+	if (!condition) {
+		fprintf(stderr, "Assertion failed: %s\n", cond_str);
+		if (msg) {
+			fprintf(stderr, "%s\n", msg);
+		}
+		fprintf(stderr, "%s(%lu):%s\n", file, line, func);
+		exit(-1);
+	}
+	return condition;
+}
+
+#define assertm(cond, msg) _assert(cond, msg, #cond, __FILE__, __func__, __LINE__)
+#define assert(cond) _assert(cond, NULL, #cond, __FILE__, __func__, __LINE__)
 
 int main(int argc, char *argv[])
 {
 	print_error("foo", "Hello, World!");
-
 	panic("asdf");
 }
