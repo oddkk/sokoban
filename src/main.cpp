ce2162cf972fd4efc0cf2ee541f682c4eadff98c
Odd Kristian Kvarmestol <okknor@gmail.com>
2018-01-29 16:13:46 +0100

    Add utility print and panic function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_error(const char *tag, const char *fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "[%s] ", tag);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	va_end(ap);
+}
+
+void panic(const char *fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "[panic] ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	va_end(ap);
+	exit(-1);
+}
+
+
 int main(int argc, char *argv[])
 {
-	printf("Hello, World!\n");
+	print_error("foo", "Hello, World!");
+
+	panic("asdf");
 }
